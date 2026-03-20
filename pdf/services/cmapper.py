25fcb90c57d2a9384f3cf71c8e8cc9f93d5737a9
Milan Kecman <milan.kecman270@gmail.com>
2026-03-20 21:14:56 +0100

    refactor(Cmapper): rename read method

--- pdf/services/cmapper.py
+++ pdf/services/cmapper.py
@@ -4,7 +4,7 @@ from django.core.files import File
 
 class Cmapper:
     DEFAULT_PNO = 1
-    DEFAULT_TEXT_FORMAT = "blocks"
+    TEXT_FORMAT_BLOCKS = "blocks"
 
     def __init__(self, file: File, page: str | int) -> None:
         self.file = file
@@ -12,10 +12,10 @@ class Cmapper:
         # Convert to proper number since first number is 1 instead of 0, because of UX
         self.page = int(page) - 1
 
-    def read(self) -> str:
+    def get_page_blocks(self) -> str:
         doc = pymupdf.open(self.file)
         page = doc[self.page]
-        blocks = page.get_text(self.DEFAULT_TEXT_FORMAT)
+        blocks = page.get_text(self.TEXT_FORMAT_BLOCKS)
 
         # (x0, y0, x1, y1, "lines in the block", block_no, block_type)
         # We only care about "lines in the block"
